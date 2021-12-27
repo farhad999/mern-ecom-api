bf6fe4ed39a76b08131fe601566ec16b2c14b444
Farhad Shikder <frd1952@gmail.com>
2021-12-27 17:35:30 +0600

    database connection using mongoose

--- api/server.js
+++ api/server.js
@@ -4,6 +4,15 @@ const dotenv = require('dotenv')
 const app = express();
 
 dotenv.config();
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=> {
+    console.log("mongo db connected");
+}).catch((er)=> {
+    console.log('Error in connection', er);
+})
 app.listen(process.env.PORT||4000, () => {
     console.log('App is rounning on port 4000');
 });
