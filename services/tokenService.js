42d0f5d3fd5a4f625cfd1c01311d292fb2a608f5
Farhad Shikder <frd1952@gmail.com>
2021-12-30 17:23:07 +0600

    add role field in token generation

--- services/tokenService.js
+++ services/tokenService.js
@@ -1,18 +1,19 @@
 const jwt = require("jsonwebtoken");
-const config = require('../configs/app.config');
+const config = require("../configs/app.config");
 const { isMatched } = require("./hashService");
 
 const generateToken = (user) => {
-  let { _id, username,email, name } = user;
+  let { _id, username, email, name, role } = user;
 
   const payload = {
     id: _id,
     username,
     email,
     name,
+    role,
   };
 
-  let token = jwt.sign(payload, config.JWT_SECRET_KEY , {
+  let token = jwt.sign(payload, config.JWT_SECRET_KEY, {
     expiresIn: config.JWT_VALIDITY,
   });
 
@@ -20,23 +21,20 @@ const generateToken = (user) => {
 };
 
 const verifyToken = (token) => {
+  let value = "";
+  let error = "";
 
-    let value='';
-    let error='';
+  try {
+    let data = jwt.verify(token, config.JWT_SECRET_KEY);
 
-    try{
-        let data = jwt.verify(token, config.JWT_SECRET_KEY);
+    let { iat, exp, ...rest } = data;
 
-        let {iat, exp, ...rest} = data;
+    value = rest;
+  } catch (er) {
+    error = er;
+  }
 
-        value = rest;
-
-    }catch(er){
-        error = er;
-    }
-
-    return {value, error};
-
+  return { value, error };
 };
 
 module.exports = {
